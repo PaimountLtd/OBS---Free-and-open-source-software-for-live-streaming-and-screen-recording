a36827ae9c05d1fca0226513b92ec807e5aaa07a
unknown <stevensoftware52@gmail.com>
2024-10-28 14:12:57 -0400

    wip

---
+++ plugins/sl-ai/sl-ai.cpp
@@ -0,0 +1,9 @@
+#include <third_party/libyuv/include/libyuv.h>
+#include <util/platform.h>
+#include <util/dstr.h>
+#include <media-io/video-frame.h>
+
+bool obs_module_load(void)
+{
+	return true;
+}
